a7cff0b484701f52e5a83b62c519c22aeb743222
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-08-05 20:35:21 +0300

    Very perfomance cariant of code

--- MovingAverageTask.cs
+++ MovingAverageTask.cs
@@ -7,20 +7,17 @@ public static class MovingAverageTask
 {
 	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		Queue<double> window = new(windowWidth);
-		DataPoint dataPoint = null;
+		Queue<double> window = new();
+        double sum = 0;
 		foreach (DataPoint point in data)
 		{
             window.Enqueue(point.OriginalY);
+            sum += point.OriginalY;
+            yield return point.WithAvgSmoothedY(sum/window.Count);
             if (window.Count == windowWidth)
             {
-                dataPoint = point.WithAvgSmoothedY(window.Average());
-                yield return dataPoint;
-                window.Dequeue();
-                continue;
-            }
-            dataPoint = point.WithAvgSmoothedY(point.OriginalY);
-		    yield return dataPoint;
+                sum = sum - window.Dequeue();
+            }   
         }
-	}
+    }
 }
