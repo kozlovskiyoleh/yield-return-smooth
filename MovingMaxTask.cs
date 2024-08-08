393736f1a488686ede91f691890dbb12449fed3c
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-08-08 20:39:06 +0300

    Tests are passed but solution is wrong

--- MovingMaxTask.cs
+++ MovingMaxTask.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace yield;
 
@@ -8,39 +9,30 @@ public static class MovingMaxTask
 {
 	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
 		LinkedList<double> window = new();
 		double max = 0;
 		
 		foreach(var point in data)
 		{
-            if (window.Count == 0)
-			{
-				yield return point.WithMaxY(point.OriginalY);
-                window.AddLast(point.OriginalY);
-				max = point.OriginalY;
-				continue;
-            }
-
             if (window.Count == windowWidth)
             {
                 window.RemoveFirst();
-                max = window.Last();
+                max = windowWidth > 1 ? window.Max() : 0;
+                
             }
+
+            window.AddLast(point.OriginalY);
 
             if (max >= point.OriginalY)
-			{                
-                yield return point.WithMaxY(max);
-                window.AddLast(point.OriginalY);
-            }
-				
-			if (max < point.OriginalY)
 			{
-                yield return point.WithMaxY(point.OriginalY);
-				max = point.OriginalY;
-                window.AddLast(point.OriginalY);
-            }
-        }
+				yield return point.WithMaxY(max);
+			}
 
+			if(max < point.OriginalY) 
+			{
+				yield return point.WithMaxY(point.OriginalY);
+				max = point.OriginalY;
+			}
+        }
 	}
 }
